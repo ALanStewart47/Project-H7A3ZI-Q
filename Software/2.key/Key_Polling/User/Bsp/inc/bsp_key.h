9a0aaa982eb2ef79f47f9bc28dd18ab70bfb633c
ALanStewart47 <843530433@qq.com>
2024-05-21 19:27:58 +0800

    修复一些错误

--- Software/2.key/Key_Polling/User/Bsp/inc/bsp_key.h
+++ Software/2.key/Key_Polling/User/Bsp/inc/bsp_key.h
@@ -62,13 +62,8 @@
 typedef enum
 {
 	KID_K1 = 0,
-	KID_K2,
-	KID_K3,
-	KID_JOY_U,
-	KID_JOY_D,
-	KID_JOY_L,
-	KID_JOY_R,
-	KID_JOY_OK
+	//KID_K2,
+	//KID_K3
 }KEY_ID_E;
 
 /*
@@ -110,42 +105,22 @@ typedef enum
 	KEY_1_UP,				/* 1键弹起 */
 	KEY_1_LONG,				/* 1键长按 */
 
-	KEY_2_DOWN,				/* 2键按下 */
-	KEY_2_UP,				/* 2键弹起 */
-	KEY_2_LONG,				/* 2键长按 */
+//	KEY_2_DOWN,				/* 2键按下 */
+//	KEY_2_UP,				/* 2键弹起 */
+//	KEY_2_LONG,				/* 2键长按 */
 
-	KEY_3_DOWN,				/* 3键按下 */
-	KEY_3_UP,				/* 3键弹起 */
-	KEY_3_LONG,				/* 3键长按 */
-
-	KEY_4_DOWN,				/* 4键按下 */
-	KEY_4_UP,				/* 4键弹起 */
-	KEY_4_LONG,				/* 4键长按 */
-
-	KEY_5_DOWN,				/* 5键按下 */
-	KEY_5_UP,				/* 5键弹起 */
-	KEY_5_LONG,				/* 5键长按 */
-
-	KEY_6_DOWN,				/* 6键按下 */
-	KEY_6_UP,				/* 6键弹起 */
-	KEY_6_LONG,				/* 6键长按 */
-
-	KEY_7_DOWN,				/* 7键按下 */
-	KEY_7_UP,				/* 7键弹起 */
-	KEY_7_LONG,				/* 7键长按 */
-
-	KEY_8_DOWN,				/* 8键按下 */
-	KEY_8_UP,				/* 8键弹起 */
-	KEY_8_LONG,				/* 8键长按 */
+//	KEY_3_DOWN,				/* 3键按下 */
+//	KEY_3_UP,				/* 3键弹起 */
+//	KEY_3_LONG,				/* 3键长按 */
 
 	/* 组合键 */
-	KEY_9_DOWN,				/* 9键按下 */
-	KEY_9_UP,				/* 9键弹起 */
-	KEY_9_LONG,				/* 9键长按 */
+//	KEY_9_DOWN,				/* 9键按下 */
+//	KEY_9_UP,				/* 9键弹起 */
+//	KEY_9_LONG,				/* 9键长按 */
 
-	KEY_10_DOWN,			/* 10键按下 */
-	KEY_10_UP,				/* 10键弹起 */
-	KEY_10_LONG,			/* 10键长按 */
+//	KEY_10_DOWN,			/* 10键按下 */
+//	KEY_10_UP,				/* 10键弹起 */
+//	KEY_10_LONG,			/* 10键长按 */
 }KEY_ENUM;
 
 /* 按键FIFO用到变量 */
