bf3b11017010497655a236ae53497e4df6d4dfe5
ALanStewart <843530433@qq.com>
2024-06-17 02:00:12 +0800

    updata Timer IC

---
+++ Software/6.Timer_PWM/TimerIC/TimerIC_TIM5/Bsp/Inc/bsp_timer_ic.h
@@ -0,0 +1,14 @@
+#ifndef	__BSP_TIMER_IC_H__
+#define	__BSP_TIMER_IC_H__
+
+#include	"stdio.h"
+#include	"stdint.h"
+#include	"tim.h"
+#include	"stm32h7xx_hal.h"
+
+
+void bsp_TIM5_CH1_Capture_Hight_Time(void);
+
+
+#endif
+/***************************	endif		******************************/
